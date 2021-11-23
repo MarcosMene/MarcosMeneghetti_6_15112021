eea6ef6113d7aab5513133f626bb813bf36da170
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2021-11-23 16:34:25 +0100

    index page affiche tout

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,54 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/photographersID/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    // const article = document.querySelector(".photographer_section");
+
+    const photoCard = document.createElement("article");
+    photoCard.className = "photographer_card";
+
+    const photoLink = document.createElement("a");
+    photoLink.setAttribute("href", "./photographer.html");
+
+    const photoImg = document.createElement("div");
+    photoImg.className = "photographer_img";
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    photoImg.appendChild(img);
+    photoImg.appendChild(h2);
+    photoLink.appendChild(photoImg);
+    photoCard.appendChild(photoLink);
+
+    const photoInfo = document.createElement("div");
+    photoInfo.className = "photographer_info";
+
+    const cityBirth = document.createElement("p");
+    cityBirth.className = "city";
+    cityBirth.textContent = `${city + ", " + country}`;
+
+    const tagLine = document.createElement("p");
+    tagLine.className = "tagline";
+    tagLine.textContent = tagline;
+
+    const priceDay = document.createElement("p");
+    priceDay.className = "price";
+    priceDay.innerHTML = `${price + "&euro;" + "/jour"}`;
+
+    photoInfo.appendChild(cityBirth);
+    photoInfo.appendChild(tagLine);
+    photoInfo.appendChild(priceDay);
+
+    photoCard.appendChild(photoLink);
+    photoCard.appendChild(photoInfo);
+
+    return photoCard;
+  }
+  return { name, picture, getUserCardDOM };
 }
