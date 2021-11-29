debdfefd121906e3056edbdcf486e8c071bf2346
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2021-11-29 14:21:40 +0100

    integration image et video dans les catalog de photographe

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,30 +1,63 @@
 function MediaPageFactory(data) {
-  const { photographerId, title, image, likes } = data;
-
+  const { id, photographerId, video, title, image, likes, date, price, alt } =
+    data;
+  console.log(data);
   const catalog = `./assets/photographers/${photographerId}/${image}`;
 
+  //list media selon id
   function MediaDOM() {
     const $wrapper = document.createElement("div");
     $wrapper.classList.add("photograph-catalog-card");
 
-    const mediaCard = `
-        
-        <img src="${catalog}" alt="">
+    let cardmedias = "";
 
-        <div class="photograph-catalog-info">
-          <div class="photograph-catalog-txt">
-            <p>${title}</p>
-          </div>
-          <div class="photograph-catalog-icon">
+    if (video !== undefined) {
+      cardmedias += `<video class="card_video" aria-label="${alt}" controls>
+      <source src="./assets/photographers/${photographerId}/${video}" type="video/mp4">
+      </video>`;
+    } else {
+      cardmedias += `<img class="card_image" src="${catalog}" alt="${alt}">`;
+    }
+    cardmedias += `
+    <div class="photograph-catalog-info">
+       <div class="photograph-catalog-txt">
+          <p>${title}</p>
+       </div>
+        <div class="photograph-catalog-icon">
             <p>${likes}</p>
-            <i class="fas fa-heart"></i>
-          </div>
-        </div>
-      </div>
-        `;
+           <i class="fas fa-heart"></i>
+         </div>
+       </div>
+    `;
+    // const mediaCard = `
 
-    $wrapper.innerHTML = mediaCard;
+    //     <img src="${catalog}" alt="">
+
+    //     <div class="photograph-catalog-info">
+    //       <div class="photograph-catalog-txt">
+    //         <p>${title}</p>
+    //       </div>
+    //       <div class="photograph-catalog-icon">
+    //         <p>${likes}</p>
+    //         <i class="fas fa-heart"></i>
+    //       </div>
+    //     </div>
+    //   </div>
+    //     `;
+
+    $wrapper.innerHTML = cardmedias;
     return $wrapper;
   }
-  return { MediaDOM };
+  return {
+    id,
+    photographerId,
+    video,
+    title,
+    image,
+    likes,
+    date,
+    price,
+    alt,
+    MediaDOM,
+  };
 }
