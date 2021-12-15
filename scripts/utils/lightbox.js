b8ab64e6beaa1fb11e10e940c1b1612eee1bcdfc
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2021-12-15 11:21:54 +0100

    lightbox finalise

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -6,11 +6,21 @@ function lightboxShow() {
   const imageSelected = document.querySelectorAll(".media_link");
 
   // click for all videos
-  for (const imgClicked of imageSelected) {
-    imgClicked.addEventListener("click", (event) => {
+  // for (const imgClicked of imageSelected) {
+  //   imgClicked.addEventListener("click", (event) => {
+  //     event.preventDefault();
+  //     const modalLightBox = document.querySelector(".lightbox_modal");
+  //     modalLightBox.style.display = "flex";
+  //     currentSlide(1);
+  //   });
+  // }
+
+  for (let i = 0; i < imageSelected.length; i++) {
+    imageSelected[i].addEventListener("click", (event) => {
       event.preventDefault();
       const modalLightBox = document.querySelector(".lightbox_modal");
       modalLightBox.style.display = "flex";
+      currentSlide(i + 1);
     });
   }
   // close lightboxmodal
@@ -20,12 +30,44 @@ function lightboxShow() {
     modalLightBox.style.display = "none";
   });
 
+  //controls lightbox prev/next
   const linkPrevLightBox = document.querySelector(".prev_image");
   linkPrevLightBox.addEventListener("click", (event) => {
     event.preventDefault();
+    plusSlides(-1);
   });
   const linkNextLightBox = document.querySelector(".next_image");
   linkNextLightBox.addEventListener("click", (event) => {
     event.preventDefault();
+    plusSlides(1);
   });
+
+  // animation of lightbox
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("modal_media");
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+  }
 }
